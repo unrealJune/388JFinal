df7dd9ca57b22c7ec8c41bc85ac5291ac7d9b8ef
unrealJune <mail.junephilip@gmail.com>
2023-04-26 19:19:57 -0400

    Update config.py

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,7 +1,9 @@
 
 import os
-my_secret = os.environ['atlas_username']
-my_pass = os.environ['atlas_password']
-# Stores all configuration values
-SECRET_KEY = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
-MONGODB_HOST = "mongodb+srv://" + my_secret + ":" + my_pass + "@final.kk2kkml.mongodb.net/?retryWrites=true&w=majority"
+# my_secret = os.environ['atlas_username']
+# my_pass = os.environ['atlas_password']
+# print(my_secret)
+# print(my_pass)
+# # Stores all configuration values
+# SECRET_KEY = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec' 
+MONGODB_HOST = os.environ['MONGOURL']
